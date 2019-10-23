2d027244b63f019816152f6c4538662ea318c854
kaizen-minded <melvin3ezell@gmail.com>
2019-10-22 23:40:15 -0400

    Bypassing props with Context

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -2,29 +2,37 @@ import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
 import Results from './Results'
-
+import { ThemeConsumer } from '../contexts/theme'
 
 function Instructions() {
     return (
-        <div className='instructions-container'>
-            <h1 className='center-text header-lg'>
-                Instructions
-            </h1>
-            <ol className='container-sm grid center-text battle-instructions'>
-                <li>
-                    <h3 className='header-sm'>Enter two Github users</h3>
-                    <FaUserFriends className='bg-light' color='rgb(255, 191, 116)' size={140} />
-                </li>
-                <li>
-                    <h3 className='header-sm'>Battle</h3>
-                    <FaFighterJet className='bg-light' color='#727272' size={140} />
-                </li>
-                <li>
-                    <h3 className='header-sm'>See the winner</h3>
-                    <FaTrophy className='bg-light' color='rgb(255, 215, 0)' size={140} />
-                </li>
-            </ol>
-        </div>
+        <ThemeConsumer>
+            {({ theme }) => {
+                return (
+                    <div className='instructions-container'>
+                        <h1 className='center-text header-lg'>
+                            Instructions
+                        </h1>
+                        <ol className='container-sm grid center-text battle-instructions'>
+                            <li>
+                                <h3 className='header-sm'>Enter two Github users</h3>
+                                <FaUserFriends className={`bg-${theme}`} color='rgb(255, 191, 116)' size={140} />
+                            </li>
+                            <li>
+                                <h3 className='header-sm'>Battle</h3>
+                                <FaFighterJet className={`bg-${theme}`} color='#727272' size={140} />
+                            </li>
+                            <li>
+                                <h3 className='header-sm'>See the winner</h3>
+                                <FaTrophy className={`bg-${theme}`} color='rgb(255, 215, 0)' size={140} />
+                            </li>
+                        </ol>
+                    </div>
+                )
+            }
+            }
+
+        </ThemeConsumer>
     )
 }
 
@@ -50,7 +58,10 @@ class PlayerInput extends React.Component {
 
     render() {
         return (
-            <form className='column player' onSubmit={this.handleSubmit}>
+            <ThemeConsumer>
+            {({ theme }) => {
+                return (
+                    <form className='column player' onSubmit={this.handleSubmit}>
                 <label htmlFor='username' className='player-label'>
                     {this.props.label}
                 </label>
@@ -58,14 +69,14 @@ class PlayerInput extends React.Component {
                     <input
                         type="text"
                         id='username'
-                        className='input-light'
+                        className={`input-${theme}`}
                         placeholder='github username'
                         autoComplete='off'
                         value={this.state.username}
                         onChange={this.handleChange}
                     />
                     <button
-                        className='btn btn-dark'
+                        className={`btn btn-${theme === 'dark' ? 'light' : 'dark'}`}
                         type='submit'
                         disabled={!this.state.username}
                     >
@@ -73,6 +84,9 @@ class PlayerInput extends React.Component {
                 </button>
                 </div>
             </form>
+                )
+            }}
+            </ThemeConsumer>
         )
     }
 }
@@ -81,39 +95,45 @@ PlayerInput.propTypes = {
     onSubmit: PropTypes.func.isRequired,
     label: PropTypes.string.isRequired
 }
- function PlayerPreview ({ username, onReset, label }) {
+function PlayerPreview({ username, onReset, label }) {
     return (
-        <div className='column player'>
-            <h3 className='player-label'>{label}</h3>
-            <div className='row bg-light'>
-                <div className='player-info'>
-                    <img 
-                        className='avatar-small'
-                        src={`https://github.com/${username}.png?size=200`}
-                        alt={`Avatar for ${username}`}
-                    />
-                    <a
-                        href={`https://github.com/${username}`}
-                        className='link'>
-                            {username}
-                        </a>
+        <ThemeConsumer>
+            {({ theme }) => {
+                return (
+                    <div className='column player'>
+                    <h3 className='player-label'>{label}</h3>
+                    <div className={`row bg-${theme}`}>
+                        <div className='player-info'>
+                            <img
+                                className='avatar-small'
+                                src={`https://github.com/${username}.png?size=200`}
+                                alt={`Avatar for ${username}`}
+                            />
+                            <a
+                                href={`https://github.com/${username}`}
+                                className='link'>
+                                {username}
+                            </a>
+                        </div>
+                        <button className="btn-clear flex-center" onClick={onReset}>
+                            <FaTimesCircle color='rgb(194,57, 42)' size={26} />
+                        </button>
+                    </div>
                 </div>
-                <button className="btn-clear flex-center" onClick={onReset}>
-                    <FaTimesCircle color='rgb(194,57, 42)' size={26} />
-                </button>
-            </div>
-        </div>
-    )   
- }
+                )
+            }}
+        </ThemeConsumer>
+    )
+}
 
- PlayerPreview.propTypes = {
-     onReset: PropTypes.func.isRequired,
-     label: PropTypes.string.isRequired
- }
+PlayerPreview.propTypes = {
+    onReset: PropTypes.func.isRequired,
+    label: PropTypes.string.isRequired
+}
 
 
 export default class Battle extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             playerOne: null,
@@ -136,19 +156,19 @@ export default class Battle extends React.Component {
     }
     render() {
         const { playerOne, playerTwo, battle } = this.state
-        
-        if(battle === true) {
+
+        if (battle === true) {
             return (
-                <Results 
-                    playerOne={playerOne} 
+                <Results
+                    playerOne={playerOne}
                     playerTwo={playerTwo}
-                    onReset={ () => this.setState({
+                    onReset={() => this.setState({
                         playerOne: null,
                         playerTwo: null,
                         battle: false
                     })}
-                    />
-                )
+                />
+            )
         }
         return (
             <React.Fragment>
@@ -157,38 +177,38 @@ export default class Battle extends React.Component {
                 <div className='players-container'>
                     <h1 className='center-text header-lg'>Players</h1>
                     <div className='row space-around'>
-                        { playerOne === null 
-                        ?<PlayerInput 
+                        {playerOne === null
+                            ? <PlayerInput
                                 label="Player One"
-                                onSubmit={(player) => this.handleSubmit('playerOne', player) }
-                                />
-                        : <PlayerPreview 
-                            username={playerOne}
-                            label='Player One'
-                            onReset={() => this.handleReset('playerOne')} />
-                        }
-                        { playerTwo === null
-                        ?<PlayerInput 
-                            label="Player Two"
-                            onSubmit={(player) => this.handleSubmit('playerTwo', player) }
+                                onSubmit={(player) => this.handleSubmit('playerOne', player)}
                             />
-                        : <PlayerPreview 
-                            username={playerTwo}
-                            label='Player Two'
-                            onReset={() => this.handleReset('playerTwo')} />
+                            : <PlayerPreview
+                                username={playerOne}
+                                label='Player One'
+                                onReset={() => this.handleReset('playerOne')} />
+                        }
+                        {playerTwo === null
+                            ? <PlayerInput
+                                label="Player Two"
+                                onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+                            />
+                            : <PlayerPreview
+                                username={playerTwo}
+                                label='Player Two'
+                                onReset={() => this.handleReset('playerTwo')} />
                         }
                     </div>
-                
-                
-                
-                {playerOne && playerTwo && (
-                    <button
-                        className="btn btn-dark btn-space"
-                        onClick={() => this.setState({battle: true})}
+
+
+
+                    {playerOne && playerTwo && (
+                        <button
+                            className="btn btn-dark btn-space"
+                            onClick={() => this.setState({ battle: true })}
                         >
                             Battle
                         </button>
-                )}
+                    )}
                 </div>
             </React.Fragment>
         )
--- app/index.js
+++ app/index.js
@@ -1,23 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from './Hello';
 import Popular from './components/Popular'
 import Battle from './components/Battle'
 import './index.css';
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
 
 // 3 concerns of Compenent
 // State
 // LifeCycle
 // UI
 
-class App extends React.Component{
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({ theme }) => ({
+                    theme: theme === 'light' ? 'dark' : 'light'
+                }))
+            }
+        }
+    }
     render() {
         return (
-            <div className='container'>
-                {/* <Hello authed html={<h1>GOod morning</h1>}firstName="Tyler" lastName="McGinnis"/> */}
-                {/* <Popular /> */}
-                <Battle />
-            </div>  
+            <ThemeProvider value={this.state} >
+                <div className={this.state.theme}>
+                    <div className='container'>
+                        <Nav />
+                        {/* <Hello authed html={<h1>GOod morning</h1>}firstName="Tyler" lastName="McGinnis"/> */}
+                        <Popular />
+                        {/* <Battle /> */}
+                    </div>
+                </div>
+
+            </ThemeProvider>
+
         )
     }
 }
@@ -25,6 +45,6 @@ class App extends React.Component{
 
 
 ReactDOM.render(
-    <App />, 
+    <App />,
     document.getElementById('app')
 )
